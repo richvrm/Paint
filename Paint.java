b0d488dec1f56c5ba10ff968778aafce842cd6f2
Pedro Achilles Carvalho <pedroachillescarvalho@hotmail.com>
2020-04-10 19:23:18 -0300

    Reta e circunferencia Brasenham
    prontos

--- Paint.java
+++ Paint.java
@@ -19,11 +19,22 @@ public class Paint extends JPanel
 	private int Rx2 = -1;
 	private int Ry2 = -1;
 
+	//variaveis das coordenadas da reta bresenham
+	private int RBx1 = -1;
+	private int RBy1 = -1;
+	private int RBx2 = -1;
+	private int RBy2 = -1;
+
+	//variaveis da circunferencia de bresenham
+	private int CBx = -1;
+	private int CBy = -1;
+	private int CBraio = 50;
+
 	private enum Ferramentas {
-		NORMAL, RETANGULO, DDA, BRESENHAM
+		NORMAL, RETANGULO, DDA, RETA_BRESENHAM, CIRC_BRESENHAM
 	};
 
-	private Ferramentas ferramenta_atual = Ferramentas.RETANGULO;
+	private Ferramentas ferramenta_atual = Ferramentas.CIRC_BRESENHAM;
 
 	public Paint()
 	{
@@ -64,6 +75,87 @@ public class Paint extends JPanel
 			Rx1 = Ry1 = Rx2 = Ry2 = -1;
 		}
 
+		public void reta_bresenham() {
+			int x, y, dx, dy, i, incrx, incry, const1, const2, p;
+			dx = RBx2 - RBx1;
+			dy = RBy2 - RBy1;
+			if (dx >= 0)
+				incrx = 1;
+			else {
+				incrx = -1;
+				dx = -dx;
+			}
+			if (dy >= 0)
+				incry = 1;
+			else {
+				incry = -1;
+				dy= -dy;
+			}
+			x = RBx1;
+			y = RBy1;
+			ponto(x, y);
+			if (dy < dx) {
+				p = 2 * dy - dx;
+				const1 = 2 * dy;
+				const2 = 2 * (dy - dx);
+				for(i = 0; i < dx; i++) {
+					x += incrx;
+					if (p < 0)
+						p += const1;
+					else {
+						y += incry;
+						p += const2;
+					}
+					ponto(x, y);
+				}
+			}
+			else {
+				p = 2 * dx - dy;
+				const1 = 2 * dx;
+				const2 = 2 * (dx - dy);
+				for(i = 0; i < dy; i++) {
+					y += incry;
+					if (p < 0)
+						p += const1;
+					else { x += incrx;
+						p += const2;
+					}
+					ponto(x, y);
+				}
+			}
+			RBx1 = RBy1 = RBx2 = RBy2 = -1;
+		}
+
+		public void colorirSimetricos(int x, int y) {
+			ponto(CBx + x, CBy + y);
+			ponto(CBx - x, CBy + y);
+			ponto(CBx + x, CBy - y);
+			ponto(CBx - x, CBy - y);
+			ponto(CBx + y, CBy + x);
+			ponto(CBx - y, CBy + x);
+			ponto(CBx + y, CBy - x);
+			ponto(CBx - y, CBy - x);
+		}
+
+		public void circunferencia_bresenham() {
+			int x, y, p;
+			x = 0;
+			y = CBraio;
+			p = 3 - 2 * CBraio;
+			colorirSimetricos(x, y);
+			while(x < y) {
+				if (p < 0)
+					p += 4 * x + 6;
+				else {
+					p += 4 * (x - y) + 10;
+					y--;
+				}
+				x++;
+				colorirSimetricos(x, y);
+			}
+			CBx = CBy = -1;
+		}
+
 		public void mousePressed( MouseEvent e )
 		{
 			x1 = e.getX();
@@ -84,6 +176,24 @@ public class Paint extends JPanel
 					//Desenha o retangulo 
 					retangulo();
 				}
+			} else if(ferramenta_atual == Ferramentas.RETA_BRESENHAM) {
+				//captura o primeiro ponto se as primeiras variaveis da
+				//reta forem -1
+				if (RBx1 == -1) {
+					RBx1 = x1;
+					RBy1 = y1;
+				//Captura o segundo ponto se as primeiras variaveis da 
+				//reta forem != -1 e as segundas forem -1
+				} else if(RBx2 == -1) {
+					RBx2 = x1;
+					RBy2 = y1;
+					//Desenha a reta bresenham
+					reta_bresenham();
+				}
+			} else if(ferramenta_atual == Ferramentas.CIRC_BRESENHAM) {
+				CBx = x1;
+				CBy = y1;
+				circunferencia_bresenham();
 			}
 			x2=x1;
 			y2=y1;
