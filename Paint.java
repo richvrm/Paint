27c8fc99de6fc5cedc1402ce5400e7aeb001d8bb
Pedro Achilles Carvalho <pedroachillescarvalho@hotmail.com>
2020-04-12 03:51:44 -0300

    Adapração ao Ponto
    Criada classe Ponto para facilitar algoritmos complexos e adaptação de todos os existentes a ele

--- Paint.java
+++ Paint.java
@@ -19,6 +19,12 @@ public class Paint extends JPanel
 	private int Rx2 = -1;
 	private int Ry2 = -1;
 
+	//variaveis das coordenadas do DDA
+	private int DDAx1 = -1;
+	private int DDAy1 = -1;
+	private int DDAx2 = -1;
+	private int DDAy2 = -1;
+
 	//variaveis das coordenadas da reta bresenham
 	private int RBx1 = -1;
 	private int RBy1 = -1;
@@ -34,7 +40,7 @@ public class Paint extends JPanel
 		NORMAL, RETANGULO, DDA, RETA_BRESENHAM, CIRC_BRESENHAM
 	};
 
-	private Ferramentas ferramenta_atual = Ferramentas.CIRC_BRESENHAM;
+	private Ferramentas ferramenta_atual = Ferramentas.NORMAL;
 
 	public Paint()
 	{
@@ -55,53 +61,76 @@ public class Paint extends JPanel
 	//Classe interna para lidar com eventos de mouse
 	private class MouseHandler extends MouseAdapter
 	{
-
-		public void dda(int x1, int y1, int x2, int y2) 
+		public void dda() 
 		{
 			int dx, dy, passos, k;
-			double x_inc, y_inc, x,y;
-			dx = x2-x1;
-			dy = y2-y1;
-			if (abs(dx) > abs(dy))
-				passos = abs(dx);
+			double x_inc, y_inc, x, y;
+			Ponto p;
+			Ponto p1 = new Ponto(DDAx1, DDAy1);
+			Ponto p2 = new Ponto(DDAx2, DDAy2);
+			dx = p2.x - p1.x;
+			dy = p2.y - p1.y;
+			if (Math.abs(dx) > Math.abs(dy))
+				passos = Math.abs(dx);
 			else
-				passos = abs(dy);
+				passos = Math.abs(dy);
 
-			x_inc = double(dx)/double(passos);
-			y_inc = double(dy)/double(passos);
+			x_inc = (double)dx / (double)passos;
+			y_inc = (double)dy / (double)passos;
 
-			x=x1;
-			y=y1;
-			drawLine(round(x),round(y),round(x),round(y));
+			x = p1.x;
+			y = p1.y;
+
+			p = new Ponto((int)Math.floor(x), (int)Math.floor(y));
+			setPixel(p);
 
 			for(k=1; k< passos; k++) {
 				x = x+x_inc;
 				y = y+y_inc;
-				drawLine(round(x),round(y),round(x),round(y));
+				p = new Ponto((int)Math.floor(x), (int)Math.floor(y));
+				setPixel(p);
 			}
+
+			DDAx1 = DDAy1 = DDAx2 = DDAy2 = -1;
 		}
 
-		public void ponto(int x,int y)
-		{
+		public void setPixel(Ponto ponto) {
 			setupDesenho();
-			g.drawLine(x,y,x,y);
+			g.drawLine(ponto.x, ponto.y, ponto.x, ponto.y);
 		}
 
 		public void retangulo() {
 			setupDesenho();
 			//Reta superior
-			g.drawLine(Rx1,Ry1,Rx2,Ry1);
+			RBx1 = Rx1;
+			RBy1 = Ry1;
+			RBx2 = Rx2;
+			RBy2 = Ry1;
+			reta_bresenham();
 			//lateral esquerda
-			g.drawLine(Rx1,Ry1,Rx1,Ry2);
+			RBx1 = Rx1;
+			RBy1 = Ry1;
+			RBx2 = Rx1;
+			RBy2 = Ry2;
+			reta_bresenham();
 			//reta inferior
-			g.drawLine(Rx1,Ry2,Rx2,Ry2);
+			RBx1 = Rx1;
+			RBy1 = Ry2;
+			RBx2 = Rx2;
+			RBy2 = Ry2;
+			reta_bresenham();
 			//lateral direita
-			g.drawLine(Rx2,Ry1,Rx2,Ry2);
+			RBx1 = Rx2;
+			RBy1 = Ry1;
+			RBx2 = Rx2;
+			RBy2 = Ry2;
+			reta_bresenham();
 			Rx1 = Ry1 = Rx2 = Ry2 = -1;
 		}
 
 		public void reta_bresenham() {
 			int x, y, dx, dy, i, incrx, incry, const1, const2, p;
+			Ponto ponto;
 			dx = RBx2 - RBx1;
 			dy = RBy2 - RBy1;
 			if (dx >= 0)
@@ -118,7 +147,8 @@ public class Paint extends JPanel
 			}
 			x = RBx1;
 			y = RBy1;
-			ponto(x, y);
+			ponto = new Ponto(x, y);
+			setPixel(ponto);
 			if (dy < dx) {
 				p = 2 * dy - dx;
 				const1 = 2 * dy;
@@ -131,7 +161,8 @@ public class Paint extends JPanel
 						y += incry;
 						p += const2;
 					}
-					ponto(x, y);
+					ponto = new Ponto(x, y);
+					setPixel(ponto);
 				}
 			}
 			else {
@@ -145,21 +176,25 @@ public class Paint extends JPanel
 					else { x += incrx;
 						p += const2;
 					}
-					ponto(x, y);
+					ponto = new Ponto(x, y);
+					setPixel(ponto);
 				}
 			}
 			RBx1 = RBy1 = RBx2 = RBy2 = -1;
 		}
 
 		public void colorirSimetricos(int x, int y) {
-			ponto(CBx + x, CBy + y);
-			ponto(CBx - x, CBy + y);
-			ponto(CBx + x, CBy - y);
-			ponto(CBx - x, CBy - y);
-			ponto(CBx + y, CBy + x);
-			ponto(CBx - y, CBy + x);
-			ponto(CBx + y, CBy - x);
-			ponto(CBx - y, CBy - x);
+			Ponto[] pontos = new Ponto[8];
+			pontos[0] = new Ponto(CBx + x, CBy + y);
+			pontos[1] = new Ponto(CBx - x, CBy + y);
+			pontos[2] = new Ponto(CBx + x, CBy - y);
+			pontos[3] = new Ponto(CBx - x, CBy - y);
+			pontos[4] = new Ponto(CBx + y, CBy + x);
+			pontos[5] = new Ponto(CBx - y, CBy + x);
+			pontos[6] = new Ponto(CBx + y, CBy - x);
+			pontos[7] = new Ponto(CBx - y, CBy - x);
+			for(int i = 0; i < pontos.length; i++)
+				setPixel(pontos[i]);
 		}
 
 		public void circunferencia_bresenham() {
@@ -185,8 +220,10 @@ public class Paint extends JPanel
 		{
 			x1 = e.getX();
 			y1 = e.getY();
+
 			if (ferramenta_atual == Ferramentas.NORMAL) {
-				ponto(x1,y1);
+				Ponto p = new Ponto(x1, y1);
+				setPixel(p);
 			} else if(ferramenta_atual == Ferramentas.RETANGULO) {
 				//captura o primeiro ponto se as primeiras variaveis do
 				//retangulo forem -1
@@ -219,6 +256,19 @@ public class Paint extends JPanel
 				CBx = x1;
 				CBy = y1;
 				circunferencia_bresenham();
+			} else if(ferramenta_atual == Ferramentas.DDA) {
+				//captura o primeiro ponto se as primeiras variaveis da
+				//reta forem -1
+				if (DDAx1 == -1) {
+					DDAx1 = x1;
+					DDAy1 = y1;
+				//Captura o segundo ponto se as primeiras variaveis da 
+				//reta forem != -1 e as segundas forem -1
+				} else if(DDAx2 == -1) {
+					DDAx2 = x1;
+					DDAy2 = y1;
+					dda();
+				}
 			}
 			x2=x1;
 			y2=y1;
@@ -230,7 +280,12 @@ public class Paint extends JPanel
 				x1 = e.getX();
 				y1 = e.getY();
 
-				g.drawLine(x1,y1,x2,y2);
+				RBx1 = x1;
+				RBy1 = y1;
+				RBx2 = x2;
+				RBy2 = y2;
+
+				reta_bresenham();
 
 				x2=x1;
 				y2=y1;
