80a3bf4e47d89f7db421faabbb036c89c749bff8
Laura Nunes de Andrade <laura.nunes.andrade@gmail.com>
2020-04-19 21:59:24 -0300

    Rotacao 99,99%

--- Retangulo.java
+++ Retangulo.java
@@ -4,16 +4,22 @@ import java.util.ArrayList;
 public class Retangulo {
 	public Ponto p1;
 	public Ponto p2;
+	public Ponto p3;
+	public Ponto p4;
 	public Color cor;
 	public static ArrayList<Retangulo> lista = new ArrayList<Retangulo>();
 	public Retangulo() {
 		this.p1 = new Ponto();
 		this.p2 = new Ponto();
+		this.p3 = new Ponto();
+		this.p4 = new Ponto();
 		this.cor = Color.BLACK;
 	}
 	public Retangulo(Ponto p1, Ponto p2, Color cor) {
 		this.p1 = p1;
 		this.p2 = p2;
+		this.p3 = new Ponto(p1.x, p2.y);
+		this.p4 = new Ponto(p2.x, p1.y);
 		this.cor = cor;
 		this.lista.add(this);
 	}
